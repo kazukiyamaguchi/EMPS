31db3e89c391dc3013bd8d9d6dec3b5339d58ba1
kazukiyamaguchi <kazu.white@gmail.com>
2019-02-12 14:01:24 +0900

    not working at pos[i].x =nan

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -96,6 +96,19 @@ public class Main : MonoBehaviour {
 
     List<GameObject> list_particle_ = new List<GameObject>();
 
+    void ChkPcl(int i)
+    {
+        if (Pos[i].x > MAX_X || Pos[i].x < MIN_X ||
+            Pos[i].y > MAX_Y || Pos[i].y < MIN_Y ||
+            Pos[i].z > MAX_Z || Pos[i].z < MIN_Z)
+        {
+            Typ[i] = GST;
+            Prs[i] = 0.0f;
+            Vector3 zero = new Vector3((float)0, (float)0, (float)0);
+            Vel[i] = zero;
+        }
+    }
+
     void RdDat(){
 
         Debug.Log("read input");
@@ -134,6 +147,9 @@ public class Main : MonoBehaviour {
                 py = (float)(double.Parse(textMessage[i].Split(' ')[4]));
                 pz = (float)(double.Parse(textMessage[i].Split(' ')[5]));
                 Vector3 p = new Vector3(px, py, pz);
+
+                Debug.Log(px + " " + py + " " + pz);
+
                 Pos.Add(p);
                 vx = (float)(double.Parse(textMessage[i].Split(' ')[6]));
                 vy = (float)(double.Parse(textMessage[i].Split(' ')[7]));
@@ -224,7 +240,8 @@ public class Main : MonoBehaviour {
         nBxy = nBx * nBy;
         nBxyz = nBx * nBy * nBz;        //解析領域内のバケット数
 
-        Debug.Log("nBxyz" + nBxyz);
+
+        
 
         for (int i = 0; i < nBxyz; i++)//
         {
@@ -240,34 +257,333 @@ public class Main : MonoBehaviour {
 
     void MkBkt()//バケット格納
     {
+        for (int i = 0; i < nBxyz; i++) { bfst[i] = -1; }
+        for (int i = 0; i < nBxyz; i++) { blst[i] = -1; }
+        for (int i = 0; i < nP; i++) { nxt[i] = -1; }
+        for (int i = 0; i < nP; i++)
+        {
+            if (Typ[i] == GST) continue;
+            int ix = (int)((Pos[i].x - MIN_X) * DBinv) + 1;
+            int iy = (int)((Pos[i].y - MIN_Y) * DBinv) + 1;
+            int iz = (int)((Pos[i].z - MIN_Z) * DBinv) + 1;
+            int ib = iz * nBxy + iy * nBx + ix;
+            int j = blst[ib];
+            blst[ib] = i;
+            if (j == -1) { bfst[ib] = i; }
+            else { nxt[j] = i; }
+        }
     }
     void Vsctrm()//粘性項と重力項から仮加速度計算
     {
+        for (int i = 0; i < nP; i++)
+        {
+            if (Typ[i] == FLD)
+            {
+                double Acc_x = 0.0; double Acc_y = 0.0; double Acc_z = 0.0;
+                double pos_ix = Pos[i].x; double pos_iy = Pos[i].y; double pos_iz = Pos[i].z;
+                double vec_ix = Vel[i].x; double vec_iy = Vel[i].y; double vec_iz = Vel[i].z;
+                int ix = (int)((pos_ix - MIN_X) * DBinv) + 1;
+                int iy = (int)((pos_iy - MIN_Y) * DBinv) + 1;
+                int iz = (int)((pos_iz - MIN_Z) * DBinv) + 1;
+                for (int jz = iz - 1; jz <= iz + 1; jz++)
+                {
+                    for (int jy = iy - 1; jy <= iy + 1; jy++)
+                    {
+                        for (int jx = ix - 1; jx <= ix + 1; jx++)
+                        {
+                            int jb = jz * nBxy + jy * nBx + jx;
+                            int j = bfst[jb];
+                            if (j == -1) continue;
+                            for (; ; )
+                            {
+                                double v0 = Pos[j].x - pos_ix;
+                                double v1 = Pos[j].y - pos_iy;
+                                double v2 = Pos[j].z - pos_iz;
+                                double dist2 = v0 * v0 + v1 * v1 + v2 * v2;
+                                if (dist2 < r2)
+                                {
+                                    if (j != i && Typ[j] != GST)
+                                    {
+                                        double dist = Mathf.Sqrt((float)dist2);
+                                        double w = wei(dist, r);
+                                        Acc_x += (Vel[j].x - vec_ix) * w;
+                                        Acc_y += (Vel[j].y - vec_iy) * w;
+                                        Acc_z += (Vel[j].z - vec_iz) * w;
+                                    }
+                                }
+                                j = nxt[j];
+                                if (j == -1) break;
+                            }
+                        }
+                    }
+                }
+                Vector3 Acc_new = new Vector3((float)(Acc_x * A1 + G_X), (float)(Acc_y * A1 + G_Y), (float)(Acc_z * A1 + G_Z));
+                Acc[i] = Acc_new;
+
+            }
+        }
     }
     void UpPcl1()//仮加速度から仮速度、仮位置計算
     {
+
         for (int i = 0; i < nP; i++)
         {
-            if (Typ[i] == 0)
+            if (Typ[i] == FLD)
             {
-                Vector3 v = new Vector3(0.001f, 0.00f, 0.0f);
-                Pos[i] = Pos[i] + v;
-                list_particle_[i].transform.position = Pos[i];
+                Vector3 Vel_add = new Vector3((float)(Acc[i].x * DT), (float)(Acc[i].y * DT), (float)(Acc[i].z * DT));
+                Vel[i] = Vel[i] + Vel_add;
+                Vector3 Pos_add = new Vector3((float)(Acc[i].x * DT * DT), (float)(Acc[i].y * DT * DT), (float)(Acc[i].z * DT * DT));
+                Pos[i] = Pos[i] + Pos_add;
+
+                Vector3 zero = new Vector3((float)0, (float)0, (float)0);
+                Acc[i] = zero;
+                ChkPcl(i);
+
+                /*
+                Vel[i * 3] += Acc[i * 3] * DT;
+                Vel[i * 3 + 1] += Acc[i * 3 + 1] * DT;
+                Vel[i * 3 + 2] += Acc[i * 3 + 2] * DT;
+
+                Pos[i * 3] += Acc[i * 3] * DT * DT;
+                Pos[i * 3 + 1] += Acc[i * 3 + 1] * DT * DT;
+                Pos[i * 3 + 2] += Acc[i * 3 + 2] * DT * DT;
+
+                Acc[i * 3] = Acc[i * 3 + 1] = Acc[i * 3 + 2] = 0.0;
+                ChkPcl(i);
+                */
             }
         }
+
 
     }
     void ChkCol()//剛体衝突計算
     {
+        for (int i = 0; i < nP; i++)
+        {
+            if (Typ[i] == FLD)
+            {
+                double mi = Dns[Typ[i]];
+                double pos_ix = Pos[i].x; double pos_iy = Pos[i].y; double pos_iz = Pos[i].z;
+                double vec_ix = Vel[i].x; double vec_iy = Vel[i].y; double vec_iz = Vel[i].z;
+                double vec_ix2 = Vel[i].x; double vec_iy2 = Vel[i].y; double vec_iz2 = Vel[i].z;
+
+                Vector3 test = Pos[i];
+                
+                Debug.Log("test:"+test.x);
+
+                int ix = (int)((pos_ix - MIN_X) * DBinv) + 1;
+                int iy = (int)((pos_iy - MIN_Y) * DBinv) + 1;
+                int iz = (int)((pos_iz - MIN_Z) * DBinv) + 1;
+                
+                for (int jz = iz - 1; jz <= iz + 1; jz++)
+                {
+                    for (int jy = iy - 1; jy <= iy + 1; jy++)
+                    {
+                        for (int jx = ix - 1; jx <= ix + 1; jx++)
+                        {
+                            int jb = jz * nBxy + jy * nBx + jx;
+                            
+                            int j = bfst[jb];
+                            if (j == -1) continue;
+                            for (; ; )
+                            {
+                                double v0 = Pos[j].x - pos_ix;
+                                double v1 = Pos[j].y - pos_iy;
+                                double v2 = Pos[j].z - pos_iz;
+                                double dist2 = v0 * v0 + v1 * v1 + v2 * v2;
+                                if (dist2 < rlim2)
+                                {
+                                    if (j != i && Typ[j] != GST)
+                                    {
+                                        double fDT = (vec_ix - Vel[j].x) * v0 + (vec_iy - Vel[j].y) * v1 + (vec_iz - Vel[j].z) * v2;
+                                        if (fDT > 0.0)
+                                        {
+                                            double mj = Dns[Typ[j]];
+                                            fDT *= COL * mj / (mi + mj) / dist2;
+                                            vec_ix2 -= v0 * fDT; vec_iy2 -= v1 * fDT; vec_iz2 -= v2 * fDT;
+                                        }
+                                    }
+                                }
+                                j = nxt[j];
+                                if (j == -1) break;
+                            }
+                        }
+                    }
+                }
+                
+                Vector3 Acc_new = new Vector3((float)vec_ix2, (float)vec_iy2, (float)vec_iz2);
+                Acc[i] = Acc_new;
+                //Acc[i].x = vec_ix2; Acc[i].y = vec_iy2; Acc[i].z = vec_iz2;
+            }
+        }
+        for (int i = 0; i < nP; i++)
+        {
+            Vel[i * 3] = Acc[i * 3]; Vel[i * 3 + 1] = Acc[i * 3 + 1]; Vel[i * 3 + 2] = Acc[i * 3 + 2];
+        }
     }
     void MkPrs()//仮圧力計算
     {
+        for (int i = 0; i < nP; i++)
+        {
+            if (Typ[i] != GST)
+            {
+                double pos_ix = Pos[i].x; double pos_iy = Pos[i].y; double pos_iz = Pos[i].z;
+                double ni = 0.0;
+                int ix = (int)((pos_ix - MIN_X) * DBinv) + 1;
+                int iy = (int)((pos_iy - MIN_Y) * DBinv) + 1;
+                int iz = (int)((pos_iz - MIN_Z) * DBinv) + 1;
+                for (int jz = iz - 1; jz <= iz + 1; jz++)
+                {
+                    for (int jy = iy - 1; jy <= iy + 1; jy++)
+                    {
+                        for (int jx = ix - 1; jx <= ix + 1; jx++)
+                        {
+                            int jb = jz * nBxy + jy * nBx + jx;
+                            int j = bfst[jb];
+                            if (j == -1) continue;
+                            for (; ; )
+                            {
+                                double v0 = Pos[j].x - pos_ix;
+                                double v1 = Pos[j].y - pos_iy;
+                                double v2 = Pos[j].z - pos_iz;
+                                double dist2 = v0 * v0 + v1 * v1 + v2 * v2;
+                                if (dist2 < r2)
+                                {
+                                    if (j != i && Typ[j] != GST)
+                                    {
+                                        
+                                        double dist = Mathf.Sqrt((float)dist2);
+                                        double w = wei(dist, r);
+                                        ni += w;
+                                    }
+                                }
+                                j = nxt[j];
+                                if (j == -1) break;
+                            }
+                        }
+                    }
+                }
+                double mi = Dns[Typ[i]];
+                double pressure = 0;
+                if (ni > n0)
+                {
+                    pressure = (ni - n0) * A2 * mi;
+                }
+                
+                //double pressure = (ni > n0) * (ni - n0) * A2 * mi;
+                Prs[i] = pressure;
+            }
+        }
     }
     void PrsGrdTrm()//圧力勾配項、修正加速度計算
     {
+        for (int i = 0; i < nP; i++)
+        {
+            if (Typ[i] == FLD)
+            {
+                double Acc_x = 0.0; double Acc_y = 0.0; double Acc_z = 0.0;
+                double pos_ix = Pos[i].x; double pos_iy = Pos[i].y; double pos_iz = Pos[i].z;
+                double pre_min = Prs[i];
+                int ix = (int)((pos_ix - MIN_X) * DBinv) + 1;
+                int iy = (int)((pos_iy - MIN_Y) * DBinv) + 1;
+                int iz = (int)((pos_iz - MIN_Z) * DBinv) + 1;
+                for (int jz = iz - 1; jz <= iz + 1; jz++)
+                {
+                    for (int jy = iy - 1; jy <= iy + 1; jy++)
+                    {
+                        for (int jx = ix - 1; jx <= ix + 1; jx++)
+                        {
+                            int jb = jz * nBxy + jy * nBx + jx;
+                            int j = bfst[jb];
+                            if (j == -1) continue;
+                            for (; ; )
+                            {
+                                double v0 = Pos[j].x - pos_ix;
+                                double v1 = Pos[j].y - pos_iy;
+                                double v2 = Pos[j].z - pos_iz;
+                                double dist2 = v0 * v0 + v1 * v1 + v2 * v2;
+                                if (dist2 < r2)
+                                {
+                                    if (j != i && Typ[j] != GST)
+                                    {
+                                        if (pre_min > Prs[j]) pre_min = Prs[j];
+                                    }
+                                }
+                                j = nxt[j];
+                                if (j == -1) break;
+                            }
+                        }
+                    }
+                }
+                for (int jz = iz - 1; jz <= iz + 1; jz++)
+                {
+                    for (int jy = iy - 1; jy <= iy + 1; jy++)
+                    {
+                        for (int jx = ix - 1; jx <= ix + 1; jx++)
+                        {
+                            int jb = jz * nBxy + jy * nBx + jx;
+                            int j = bfst[jb];
+                            if (j == -1) continue;
+                            for (; ; )
+                            {
+                                double v0 = Pos[j].x - pos_ix;
+                                double v1 = Pos[j].y - pos_iy;
+                                double v2 = Pos[j].z - pos_iz;
+                                double dist2 = v0 * v0 + v1 * v1 + v2 * v2;
+                                if (dist2 < r2)
+                                {
+                                    if (j != i && Typ[j] != GST)
+                                    {
+                                        double dist = Mathf.Sqrt((float)dist2);
+                                        double w = wei(dist, r);
+                                        w *= (Prs[j] - pre_min) / dist2;
+                                        Acc_x += v0 * w; Acc_y += v1 * w; Acc_z += v2 * w;
+                                    }
+                                }
+                                j = nxt[j];
+                                if (j == -1) break;
+                            }
+                        }
+                    }
+                }
+                Vector3 Acc_new = new Vector3((float)(Acc_x * invDns[FLD] * A3), (float)(Acc_y * invDns[FLD] * A3), (float)(Acc_z * invDns[FLD] * A3));
+                Acc[i] = Acc_new;
+
+                //Acc[i] = Acc_x * invDns[FLD] * A3;
+                //Acc[i * 3 + 1] = Acc_y * invDns[FLD] * A3;
+                //Acc[i * 3 + 2] = Acc_z * invDns[FLD] * A3;
+            }
+        }
     }
     void UpPcl2()//修正加速度から時刻更新後の位置と速度計算
     {
+        for (int i = 0; i < nP; i++)
+        {
+            if (Typ[i] == FLD)
+            {
+                Vector3 Vel_add = new Vector3((float)(Acc[i].x * DT), (float)(Acc[i].y * DT), (float)(Acc[i].z * DT));
+                Vel[i] = Vel[i]+Vel_add;
+                Vector3 Pos_add = new Vector3((float)(Acc[i].x * DT * DT), (float)(Acc[i].y * DT * DT), (float)(Acc[i].z * DT * DT));
+                Pos[i] = Pos[i] + Pos_add;
+
+                Vector3 zero= new Vector3((float)0, (float)0, (float)0);
+                Acc[i] = zero;
+                ChkPcl(i);
+
+                /*
+                Vel[i * 3] += Acc[i * 3] * DT;
+                Vel[i * 3 + 1] += Acc[i * 3 + 1] * DT;
+                Vel[i * 3 + 2] += Acc[i * 3 + 2] * DT;
+
+                Pos[i * 3] += Acc[i * 3] * DT * DT;
+                Pos[i * 3 + 1] += Acc[i * 3 + 1] * DT * DT;
+                Pos[i * 3 + 2] += Acc[i * 3 + 2] * DT * DT;
+
+                Acc[i * 3] = Acc[i * 3 + 1] = Acc[i * 3 + 2] = 0.0;
+                ChkPcl(i);
+                */
+            }
+        }
     }
 
     void ClcEMPS()
@@ -314,8 +630,7 @@ public class Main : MonoBehaviour {
             var obj = GameObject.Instantiate(Sphere,Pos[i] , Quaternion.identity) as GameObject;
             list_particle_.Add(obj);
         }
-        Debug.Log(Pos.Count());
-        Debug.Log(Typ.Count());
+        
         SetPara();
         AlcBkt();
         
@@ -345,7 +660,20 @@ public class Main : MonoBehaviour {
             {
                 pav[i] = Prs[i];
             }
-            iLP++;
+
+            //描画
+            for (int i = 0; i < nP; i++)
+            {
+            if (Typ[i] == FLD)
+                {
+                    //Vector3 v = new Vector3(0.001f, 0.00f, 0.0f);
+                    //Pos[i] = Pos[i] + v;
+                    list_particle_[i].transform.position = Pos[i];
+                }
+            }
+
+
+        iLP++;
             TIM += DT;
 
         
